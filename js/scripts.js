bed05dce9337814b08ea2a5d3f8ac9b15bb91226
nn <ninain@gmail.com>
2019-02-18 23:06:38 -0800

    add date and time input types to form

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+$("#blanks form").submit(function(event) {
+    event.preventDefault();
+  var personInput = $("input#person").val();
+  var addressInput= $("input#address").val();
+  var dateInput = $("input#date").val();
+  var startTimeInput = $("input#start-time").val();
+  var endTimeInput = $("input#end-time").val();
+
+
+
+    $(".person").text(personInput);
+    $(".address").text(addressInput);
+    $(".date").text(dateInput);
+    $(".start-time").text(startTimeInput);
+    $(".end-time").text(endTimeInput);
+
+    $("#confirmation").show();
+    $("#blanks").hide();
+
+
+});
+});
